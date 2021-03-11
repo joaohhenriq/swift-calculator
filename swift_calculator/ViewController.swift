d8e719a9202423c14b294b4a4b6f25575672c3ea
joao henrique <joao_camargo10@hotmail.com>
2021-03-11 20:17:28 -0300

    Calculate method

--- swift_calculator/ViewController.swift
+++ swift_calculator/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtFirstNumber: UITextField!
+    @IBOutlet weak var txtSecondNumber: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func calculate(_ sender: Any) {
+        
+        let n1 = Double(txtFirstNumber.text!)!
+        let n2 = Double(txtSecondNumber.text!)!
+        
+        let result = n1 + n2
+        
+        lbResult.text = String(result)
+    }
 }
 
